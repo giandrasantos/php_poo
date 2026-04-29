869798bd73f5a9adb7e06e387015bfdfafe6801a
giandrasantos <giandra.santos@aluno.senai.br>
2026-04-29 14:19:43 -0300

    exemplo 19

--- exemplo19.php
+++ exemplo19.php
@@ -1,6 +1,7 @@
 <?php
 
-abstract class FiguraGeometrica{
+abstract class FiguraGeometrica
+{
     private $tipo;
 
     public function __construct($tipo)
@@ -20,12 +21,13 @@ abstract class FiguraGeometrica{
     public abstract function perimetro();
 }
 
-class Circunferencia extends FiguraGeometrica{
+class Circunferencia extends FiguraGeometrica
+{
     private $raio;
 
     public function __construct($tipo, $raio)
     {
-        parent:: __construct($tipo);
+        parent::__construct($tipo);
         $this->raio = $raio;
     }
 
@@ -39,3 +41,32 @@ class Circunferencia extends FiguraGeometrica{
         return 2 * 3.14 * $this->raio;
     }
 }
+
+class Retangulo extends FiguraGeometrica
+{
+    private $lado1;
+    private $lado2;
+
+    public function __construct($tipo, $lado1, $lado2)
+    {
+        parent::__construct($tipo);
+        $this->lado1 = $lado1;
+        $this->lado2 = $lado2;
+    }
+
+    public function area()
+    {
+        return $this->lado1 * $this->lado2;
+    }
+
+    public function perimetro()
+    {
+        return (2 * $this->lado1) + (2 * $this->lado2);
+    }
+}
+
+$circ = new Circunferencia("Circunferencia", 10);
+$circ->printCaracteristicas();
+
+$retangulo = new Retangulo("Retangulo", 10, 20);
+$retangulo->printCaracteristicas();
